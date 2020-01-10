37acbf4224ede611f86ce93105b3244200b1d7f7
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2020-01-10 16:24:19 +0000

    Add a relational query

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,29 +2,9 @@ const { prisma } = require("./client");
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user with a new post
-  const newUser = await prisma.createUser({
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join us for GraphQL Conf in 2019"
-        },
-        {
-          title: "Subscribe to GraphQL Weekly for GraphQL news"
-        }
-      ]
-    }
-  });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  console.log(allUsers);
-
-  const allPosts = await prisma.posts();
-  console.log(allPosts);
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: "bob@prisma.io" }).posts();
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
 }
 
 main().catch(e => console.error(e));
