126e57fdbe3615e6a06a42c244c81c452c7d50ef
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2020-01-13 16:14:09 +0000

    Remove unused Mutations and Add Published Posts Query

--- packages/api/resolvers/Query.js
+++ packages/api/resolvers/Query.js
@@ -1,7 +1,15 @@
 const Query = {
+  //
+  publishedPosts(root, args, context) {
+    return context.prisma.posts({ where: { published: true } });
+  },
+
+  //
   post(root, args, context) {
     return context.prisma.post({ id: args.postId });
   },
+
+  //
   postsByUser(root, args, context) {
     return context.prisma
       .user({
