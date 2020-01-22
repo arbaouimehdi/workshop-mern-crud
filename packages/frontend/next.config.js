69aa1ca39272a75b8404cde04b6ca0f1c6b9dfba
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2020-01-22 17:27:39 +0000

    Update GraphQL Yoga URL

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    BACKEND_URI: "https://mern-crud-api-nz92wpxov.now.sh/" // insert your GraphQL Yoga url here
+    BACKEND_URI: "https://mern-crud-api.now.sh/" // insert your GraphQL Yoga url here
   }
 };
